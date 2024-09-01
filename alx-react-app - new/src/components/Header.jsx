b935f0af4a5b77a88918b498e14f8fd8289107c0
Lindah <linihim03@gmail.com>
2024-09-01 23:24:29 +0300

    Implement advanced routing with React Router.

--- alx-react-app - new/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-function Header () {
-    return(
-        <header style={{backgroundColor: 'navy', color: 'White',  textAlign: 'center' }}>
-            <h1>My Favorite Cities</h1>
-        </header>
-    );
-}
-
-export default Header;
