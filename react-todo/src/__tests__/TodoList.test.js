888338c19480a962267fc9f599a8e4f2a105bb8a
Lindah <linihim03@gmail.com>
2024-09-03 15:38:54 +0300

    Implement advanced routing with React Router.

--- react-todo/src/__tests__/TodoList.test.js
+++ react-todo/src/__tests__/TodoList.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
-import TodoList from '../TodoList';
+import TodoList from './TodoList';
 
 describe('TodoList', () => {
   it('renders TodoList component', () => {
