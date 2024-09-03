3741ebb635477daa758620ebf2f8ae22c23f2905
Lindah <linihim03@gmail.com>
2024-09-03 20:44:31 +0300

    Implement advanced routing with React Router.

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './components/Home';
-import Profile from './components/Profile';
+import Profile from './components/profile';
 import BlogPost from './components/BlogPost';
 import Login from './components/Login';
 import ProtectedRoute from './components/ProtectedRoute';
@@ -12,7 +12,7 @@ function App() {
       <div>
         <nav>
           <ul>
-            <li><Link to="/">Home</Link></li>
+          <li><Link to="/">Home</Link></li>
             <li><Link to="/profile">Profile</Link></li>
             <li><Link to="/blog/1">Blog Post 1</Link></li>
             <li><Link to="/login">Login</Link></li>
